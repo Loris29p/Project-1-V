3ee2681ab236935a345bf18f94ccefc86c4e647c
Dr. Moon 29 <pcacerloris@gmail.com>
2022-08-29 11:27:29 +0200

    2

---
+++ Src/assets/script/test.js
@@ -0,0 +1,1 @@
+// $(document.body).css("zoom", document.body.clientWidth/1920.0)
--- vpc.php
+++ vpc.php
@@ -43,6 +43,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <link rel="stylesheet" href="./Src/assets/css/gojs.css">
+        <link rel="stylesheet" href="./Src/assets/css/navbar.css">
         <script src="https://unpkg.com/gojs/release/go-debug.js"></script>
         <link rel="stylesheet" href="./Src/assets/css/main.css"> 
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
@@ -52,15 +53,34 @@
 
         <header>
             <nav class="navbar">
-                <div class="navbar-ul">
+                <!-- <div class="navbar-ul">
                     <ul>
                         <li><a href="./index.php">Acceuil</a></li>
                         <li><a href="./vpc.php">Elements</a></li>
                         <li><a href="./update.php">Mise à jour</a></li>
-                    </ul>
+                    </ul> -->
                 </div>
             </nav>
-            <div id="dropdown-cloud">
+
+            
+            <nav class="navbar2">
+                <div class="icon-menu">
+                    <i class="fal fa-bars"></i>
+                </div>
+                <!-- <div class="navbar2-ul">
+                    <ul>
+                        <li>
+                            <i class="fal fa-server"></i>
+                            <a href="./index.php">Acceuil</a>
+                        </li>
+                        <li>
+                            <i class="fal fa-server"></i>
+                            <a href="./index.php">Test</a>
+                        </li>
+                    </ul>
+                </div> -->
+            </nav>
+            <!-- <div id="dropdown-cloud">
                 <button class="dropbtn-cloud">
                     Cloud
                     <i class="fa fa-caret-down"></i>
@@ -94,9 +114,9 @@
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
-            <?php if (isset($_GET['account'])) {
+            <!-- <?php if (isset($_GET['account'])) {
                 ?>
                 <div id="dropdown-elements">
                     <button class="dropbtn-elements">
@@ -132,77 +152,9 @@
                 </div>
                 <?php
             }
-            ?>
+            ?> -->
 
-
-            <!-- Part for just draw the diagram -->
-            <?php 
-                if (isset($_GET['vpc'])) {
-                    $vpc_id = "'".$_GET['vpc']."'";
-                    ?>
-                        <div id="vpc_show_aws">
-                            <a>
-                                <i class="fab fa-aws"></i>
-                                AWS Cloud
-                            </a>
-                            <div class="zone_name">
-                                <a>
-                                    <i class="fal fa-flag"></i>
-                                    <?php echo GetInfosOfVPC($_GET['vpc'])['region']; ?>
-                                </a>
-                                <div class="vpc_info">
-                                    <a>
-                                    <i class="fal fa-network-wired"></i>
-                                        <?php echo GetInfosOfVPC($_GET['vpc'])['vpc']; ?>
-                                        <p><?php echo GetInfosOfVPC($_GET['vpc'])['cidr']; ?></p>
-                                    </a>
-                                    <div id="myDiagramDiv"></div>
-                                    <script src="./Src/assets/script/gojs.js"></script>
-                                    <div>
-                                        <script>
-                                            Construct(<?php echo json_encode($vpcArray); ?>, <?php echo json_encode($transitGatewayArray); ?>);
-                                            ConstructFirstPartVPCId(<?php echo $vpc_id; ?>);
-                                        </script>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php
-                } elseif (isset($_GET['account']) && !isset($_GET['vpc']) && !isset($_GET['transit_gateway'])) {
-                ?>
-                    <div id="vpc_show_aws">
-                        <a>
-                            <i class="fab fa-aws"></i>
-                            AWS Cloud
-                        </a>
-                        <div id="myDiagramDiv"></div>
-                        <script src="./Src/assets/script/gojs.js"></script>
-                        <div>
-                            <script>
-                                Construct(<?php echo json_encode($vpcArray); ?>, <?php echo json_encode($transitGatewayArray); ?>, true);
-                            </script>
-                        </div>
-                    </div>
-                <?php
-                } elseif (isset($_GET['transit_gateway'])) {
-                    $transit_gateway_id = "'".$_GET['transit_gateway']."'";
-                    ?>
-                        <div id="vpc_show_aws">
-                            <a>
-                                <i class="fab fa-aws"></i>
-                                AWS Cloud
-                            </a>
-                            <div id="myDiagramDiv"></div>
-                            <script src="./Src/assets/script/gojs.js"></script>
-                            <div>
-                                <script>
-                                    Construct(<?php echo json_encode($vpcArray); ?>, <?php echo json_encode($transitGatewayArray); ?>);
-                                    ConstructFirstPartTransitGatewayId(<?php echo $transit_gateway_id; ?>);
-                                </script>
-                            </div>
-                        </div>
-                    <?php
-                }
-            ?>
         </header>
     </body>
+
+    <script src="./Src/assets/script/test.js"></script>
